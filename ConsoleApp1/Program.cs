4178c610d04be61dda58420d7273bd3588e7e9f9
Vitaliy Polyanskiy <vitaliypolyanskiy@gmail.com>
2020-01-27 15:06:51 +0200

    Add pause to program.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Code is change!");
+            Console.ReadLine();
         }
     }
 }
