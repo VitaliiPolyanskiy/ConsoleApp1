e9fe6e1dc87f98a585240d208c5b4cbe441952d0
Vitaliy Polyanskiy <vitaliypolyanskiy@gmail.com>
2020-01-27 14:59:59 +0200

    Add message "Code is change!" to console output.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Code is change!");
         }
     }
 }
